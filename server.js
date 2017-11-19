24d6f078a9fa2999b28c9759d1c845edb389d1b9
aviflombaum <avi@flombaum.com>
2017-11-19 09:32:11 -0500

    Live reload

--- server.js
+++ server.js
@@ -3,24 +3,6 @@ const util = require('util');
 const logger = require('morgan');
 app.use(logger('dev'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 if(process.env.NODE_ENV === 'test'){
   app.listen(3001)
 } else {
@@ -35,8 +17,6 @@ if(process.env.NODE_ENV === 'test'){
 
     console.log(setupMessageStart, colorizedTestingAddress, setupMessageEnd);
     console.log(exitMessage);
-
-    // console.log(`Visit: http://${process.env["HOST_IP"]}:${process.env["RAILS_PORT"]}`)
   } else {
     console.log(`Visit: http://localhost:3000`)
   }
