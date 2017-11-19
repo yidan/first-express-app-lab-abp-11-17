1d7b6105d66949ffacb278eb1c0fb43869ead346
aviflombaum <avi@flombaum.com>
2017-11-19 09:27:33 -0500

    Logging on

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const app = require('./app.js')
 const util = require('util');
+const logger = require('morgan');
+app.use(logger('dev'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 if(process.env.NODE_ENV === 'test'){
   app.listen(3001)
